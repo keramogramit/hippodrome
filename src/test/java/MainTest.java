99f26b6cd195a58c2a585eb3e8fdfb69f85aeaa1
Love Zubakha <keramogramit@gmail.com>
2024-11-11 17:11:06 +0300

    ADD mainTest

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+
+
+class MainTest {
+    @Disabled
+    @Test
+    @Timeout(value = 22)
+    public void mainTest() throws Exception {
+    Main.main(null);
+}
+}
